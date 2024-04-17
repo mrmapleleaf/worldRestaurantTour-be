70b4bb5f3fdd3cd045a7651d9374095e97d0f2e7
mrmapleleaf <kaple.de.96@gmail.com>
2024-04-17 10:42:13 +0900

    新規作成

---
+++ src/test/java/com/pj/worldRestaurantTourbe/WorldRestaurantTourBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pj.worldRestaurantTourbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldRestaurantTourBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
