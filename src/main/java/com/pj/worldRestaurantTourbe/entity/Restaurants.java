06d4134c008e1c81b5a4aaa30381c8927e86f578
mrmapleleaf <kaple.de.96@gmail.com>
2024-11-19 00:35:41 +0900

    レストラン用エンティティを追加

--- src/main/java/com/pj/worldRestaurantTourbe/entity/Restaurants.java
+++ src/main/java/com/pj/worldRestaurantTourbe/entity/Restaurants.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
-public class Countries {
+public class Restaurants {
 
     @Id
     private int id;
@@ -17,15 +17,15 @@ public class Countries {
     @Column(name = "name")
     private String name;
 
-    @Column(name = "next")
-    private boolean next;
+    @Column(name = "thoughts")
+    private String thoughts;
+
+    @Column(name = "url")
+    private String url;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "restaurant_id")
-    private Restaurants restaurants;
-
-    @Column(name = "completed")
-    private boolean completed;
+    @JoinColumn(name = "country_id")
+    private Countries countries;
 
     @Column(name = "created_at")
     private LocalDateTime created_at;
--- src/test/java/com/pj/worldRestaurantTourbe/service/CountryServiceTest.java
+++ src/test/java/com/pj/worldRestaurantTourbe/service/CountryServiceTest.java
@@ -35,6 +35,7 @@ class CountryServiceTest {
         country = new Countries();
         country.setId(1);
         country.setName("日本");
+        country.setRestaurants(null);
         country.setNext(false);
         country.setCompleted(false);
     }
