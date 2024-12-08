224f44909c616c7fcf7edf0a080991576b61a0c1
mrmapleleaf <kaple.de.96@gmail.com>
2024-12-09 00:46:52 +0900

    レストラン削除エンドポイントのサービス層のテストを実装

--- src/test/java/com/pj/worldRestaurantTourbe/service/RestaurantServiceTest.java
+++ src/test/java/com/pj/worldRestaurantTourbe/service/RestaurantServiceTest.java
@@ -4,6 +4,7 @@ import com.pj.worldRestaurantTourbe.repository.CountryRepository;
 import com.pj.worldRestaurantTourbe.repository.RestaurantRepository;
 import com.pj.worldRestaurantTourbe.type.entity.Countries;
 import com.pj.worldRestaurantTourbe.type.entity.Restaurants;
+import com.pj.worldRestaurantTourbe.type.error.RestaurantNotFoundException;
 import com.pj.worldRestaurantTourbe.type.form.CompletedRestaurantFrom;
 import com.pj.worldRestaurantTourbe.type.response.RestaurantResponse;
 import org.junit.jupiter.api.DisplayName;
@@ -17,6 +18,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -38,7 +41,7 @@ public class RestaurantServiceTest {
 
         @Test
         @DisplayName("正常系")
-        public void register() {
+        public void registerRestaurant() {
 
             // prepare test data
             CompletedRestaurantFrom form = new CompletedRestaurantFrom();
@@ -72,4 +75,65 @@ public class RestaurantServiceTest {
         }
     }
 
+    @Nested
+    @DisplayName("レストラン詳細取得")
+    class GetRestaurant {
+        @Test
+        @DisplayName("正常系")
+        public void testGetRestaurantDetailWhenTargetRestaurantExists() {
+
+        }
+
+        @Test
+        @DisplayName("正常系")
+        public void testGetRestaurantDetailWhenTargetRestaurantDoesNotExistThrowException() {
+
+        }
+    }
+
+    @Nested
+    @DisplayName("レストラン削除")
+    class DeleteRestaurant {
+
+        @Test
+        @DisplayName("正常系")
+        public void deleteRestaurantWhenTargetRestaurantExists() {
+
+            // prepare test data
+            int id = 1;
+
+            Restaurants targetRestaurant = new Restaurants();
+            targetRestaurant.setId(id);
+            targetRestaurant.setName("レストラン");
+            targetRestaurant.setThoughts("good");
+            targetRestaurant.setUrl("http://sample.com");
+            Optional<Restaurants> optionalRestaurant = Optional.of(targetRestaurant);
+
+            // set mock
+            when(restaurantRepositoryMock.findById((long) id)).thenReturn(optionalRestaurant);
+
+            // execute target method
+            RestaurantResponse response = restaurantService.delete(id);
+
+            // assert response
+            assertNotNull(response.getRestaurant());
+            assertEquals(targetRestaurant.getId(), response.getRestaurant().getId());
+        }
+
+        @Test
+        @DisplayName("正常系")
+        public void testDeleteRestaurantWhenTargetRestaurantDoesNotExistThrowException() {
+
+            // prepare test data
+            int id = 999;
+
+            // set mock
+            when(restaurantRepositoryMock.findById((long) id)).thenThrow(
+                    RestaurantNotFoundException.class
+            );
+
+            // execute target method and assert result
+            assertThrows(RestaurantNotFoundException.class, () -> restaurantService.delete(id));
+        }
+    }
 }
